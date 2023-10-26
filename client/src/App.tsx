be2ddfb1340abf7a10ee532e8eeae321fa9ce055
AmreetKumarkhuntia <amreetleo123@gmail.com>
2023-10-27 01:14:09 +0530

    Added Animators

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import Home from './Pages/Home';
-import SlideUp from './Components/Animators/SlideUp';
 
 function App() {
   return (
     <div className="App">
-      {/* <Home /> */}
-      <SlideUp>HOME</SlideUp>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     </div>
   );
 }
