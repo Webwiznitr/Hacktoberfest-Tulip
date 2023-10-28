a8517d28116bcde66b9892878a79bb65b343dca1
d982h8st7 <sinnarver15@gmail.com>
2023-10-28 09:53:34 +0900

    Convert JS files to TypeScript

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,14 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import routes from './routes/routes.js';
-import cors from "cors"; 
+import routes from "./routes/routes";
+import cors from "cors";
 
 // Load environment variables from a .env file if it exists
 dotenv.config();
 
 // Create an Express application
-const app = express();
+const app: express.Express = express();
 const port = process.env.PORT || 5001;
 
 // Apply middleware
@@ -21,13 +21,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Use the route defined in the routes folder
-app.use('/api', routes);
+app.use("/api", routes);
 
 app.get("/", (req, res) => {
-  res.json("Server is running!");
+    res.send("Server is running!<br>Looks everything's fine.");
 });
 
 // Start the Express server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  console.log(`Refactoring Completed.`);
 });
