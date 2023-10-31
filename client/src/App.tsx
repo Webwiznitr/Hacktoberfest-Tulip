36ecf2910d77da37e18a079ac5bc5cd377d7c474
d982h8st7 <sinnarver15@gmail.com>
2023-10-31 10:15:41 +0900

    Adjust App.tsx import paths based on the project's directory structure.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import './CSS/index.scss'
+import './CSS/index.scss';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
